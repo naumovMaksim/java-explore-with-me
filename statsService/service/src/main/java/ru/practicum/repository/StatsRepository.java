eec12e07bb611a505840663350828d715ac4c248
Наумов Максим <dusa11223344@yandex.ru>
2023-07-20 22:51:01 +0300

    Исправлено не правильное поведение sql запросов

--- statsService/service/src/main/java/ru/practicum/repository/StatsRepository.java
+++ statsService/service/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -23,7 +23,7 @@ public interface StatsRepository extends JpaRepository<Stats, Long> {
             "ORDER BY COUNT(s.ip)")
     Long getStatsCount(LocalDateTime start, LocalDateTime end, String ip);
 
-    @Query("SELECT new ru.practicum.dto.ViewStatsDto(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
+    @Query("SELECT new ru.practicum.dto.ViewStatsDto(s.app, s.uri, COUNT(s.ip)) " +
             "FROM Stats AS s " +
             "WHERE s.timestamp BETWEEN ?1 AND ?2 " +
             "GROUP BY s.app, s.uri " +
@@ -32,12 +32,12 @@ public interface StatsRepository extends JpaRepository<Stats, Long> {
 
     @Query("SELECT new ru.practicum.dto.ViewStatsDto(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
             "FROM Stats AS s " +
-            "WHERE s.timestamp BETWEEN ?1 AND ?2 AND s.uri IN ?3" +
+            "WHERE s.timestamp BETWEEN ?1 AND ?2 AND s.uri IN (?3)" +
             "GROUP BY s.app, s.uri " +
             "ORDER BY COUNT(DISTINCT s.ip) DESC")
     List<ViewStatsDto> getAllWithUrisAndUniq(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("SELECT new ru.practicum.dto.ViewStatsDto(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
+    @Query("SELECT new ru.practicum.dto.ViewStatsDto(s.app, s.uri, COUNT(s.ip)) " +
             "FROM Stats AS s " +
             "WHERE s.timestamp BETWEEN ?1 AND ?2 " +
             "AND s.uri IN (?3) " +
